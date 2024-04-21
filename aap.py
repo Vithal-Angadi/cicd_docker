89e630569cbbfd5b3221c79829231a7c1cced97e
Vithal-Angadi <159578182+Vithal-Angadi@users.noreply.github.com>
2024-04-21 14:22:05 +0530

    Update aap.py

--- aap.py
+++ aap.py
@@ -1,16 +1,21 @@
 from flask import Flask
 import socket
+
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
 @app.route('/name')
 def name():
     return 'ITD Devops'
+
 @app.route('/ip')
 def ip():
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname) 
     return ip_address
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0",port="5000")
